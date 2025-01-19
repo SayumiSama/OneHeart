0b4cad726eb08289c0d31579f45af3494c59ae12
Louis <louis.genzel@gmail.com>
2025-01-19 18:59:31 +0100

    Added ItemClass

---
+++ src/main/java/net/sayumi/oneheart/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.sayumi.oneheart.item;
+
+import net.sayumi.oneheart.OneHeart;
+
+public class ModItems {
+
+    public static void registerModItems(){
+        OneHeart.LOGGER.info("Registering Mod Items for " + OneHeart.MOD_ID);
+    }
+}
